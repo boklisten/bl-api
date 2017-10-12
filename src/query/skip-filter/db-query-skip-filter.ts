ed671a7c2ba28cad283eecf5916b559e44eabe39
holskil <aholskil@gmail.com>
2017-10-12 15:44:01 +0200

    ADDED DbQuerySkipFilter class

---
+++ src/query/skip-filter/db-query-skip-filter.ts
@@ -0,0 +1,32 @@
+
+
+
+export type SkipFilter = {
+	skip: number;
+}
+
+
+export class DbQuerySkipFilter {
+	constructor() {}
+
+	public getSkipFilter(query: any): SkipFilter {
+		if (!query || Object.keys(query).length === 0 && query.constructor === Object) {
+			throw new TypeError('query can not be undefined or empty');
+		}
+
+		if (!query.skip) return {skip: 0};
+
+		return {skip: this.getSkipNumber(query.skip)}
+	}
+
+
+	private getSkipNumber(skip: any): number {
+		let skipstr = skip.toString();
+
+		for (let s of skipstr) {
+			if (!parseInt(s)) throw new TypeError('skip parameter "' + skip + '" is not a valid number');
+		}
+
+		return parseInt(skipstr);
+	}
+}
