3437690999335b83aefcee9a70e92aaaf97a5773
holskil <aholskil@gmail.com>
2017-12-09 14:30:49 +0100

    fix: SaltGenerator now rejects with BlError

--- src/auth/local/salt/salt-generator.ts
+++ src/auth/local/salt/salt-generator.ts
@@ -12,7 +12,12 @@ export class SaltGenerator {
 	public generate(): Promise<string> {
 		return new Promise((resolve, reject) => {
 			crypto.randomBytes(256, (error, buffer) => {
-				if (error) reject(new BlError('could not create random bytes'));
+				if (error) reject(
+					new BlError('could not create random bytes')
+						.data(error)
+						.className('SaltGenerator')
+						.methodName('generate'));
+				
 				resolve(buffer.toString('hex'));
 			});
 		});
