387af7794ad1c7e73110c567cd71da0f655c8c29
holskil <aholskil@gmail.com>
2018-01-03 11:15:38 +0100

    feat(email):  templateInput class for general email template inputs

--- src/config/secrets.ts
+++ src/config/secrets.ts
@@ -10,6 +10,11 @@ export const secrets = {
 			clientId: '1831911053785809',
 			secret: '2c15b16e445873cfb65860c3f3a95efa'
 		}
+	},
+	email: {
+		sendgrid: {
+			apiKey: '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'
+		}
 	}
 };
 
