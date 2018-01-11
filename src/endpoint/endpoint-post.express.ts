2aa3ffd8b1bb696400d52619df22af41e6d1fd26
holskil <aholskil@gmail.com>
2018-01-11 16:18:55 +0100

    fix(hook): endpointPost now handles hooks

--- src/endpoint/endpoint-post.express.ts
+++ src/endpoint/endpoint-post.express.ts
@@ -1,36 +1,32 @@
 
 import {SEResponseHandler} from "../response/se.response.handler";
-import {JwtPayload, SEToken} from "../auth/token/se.token";
 import {EndpointMongodb} from "./endpoint.mongodb";
 import {Request, Response, Router} from "express";
-import {LoginOption, Method, Path} from "./endpoint.express";
+import {Method} from "./endpoint.express";
 import * as passport from "passport";
-import {BlapiResponse, BlapiErrorResponse} from 'bl-model';
+import {BlapiResponse} from 'bl-model';
 import {SEDocument} from "../db/model/se.document";
 import {BlError} from "../bl-error/bl-error";
 import {AccessToken} from "../auth/token/access-token/access-token";
+import {Hook} from "../hook/hook";
 
 export class EndpointPostExpress {
 	private resHandler: SEResponseHandler;
-	private seToken: SEToken;
 	private endpointMongoDb: EndpointMongodb;
 
 	constructor(resHandler: SEResponseHandler, endpointMongoDb: EndpointMongodb) {
 		this.resHandler = resHandler;
-		this.seToken = new SEToken();
 		this.endpointMongoDb = endpointMongoDb;
-
 	}
 
 	public createPostEndpoint(router: Router, method: Method, url: string, collectionName: string) {
 		if (method.login && method.loginOptions) {
-			this.createLoginPost(router, url, collectionName, method.loginOptions);
+			this.createLoginPost(router, url, collectionName, method);
 		}
 	}
 
-	private createLoginPost(router: Router, url: string, collectionName: string,  loginOptions: LoginOption) {
+	private createLoginPost(router: Router, url: string, collectionName: string,  method: Method) {
 		router.post(url, passport.authenticate('jwt'), (req: Request, res: Response) => {
-			let blError = new BlError('').className('EndpointPostExpress').methodName('loginPost');
 			const accessToken: AccessToken = req.user.accessToken;
 			
 			if (!accessToken) return this.resHandler.sendErrorResponse(res, new BlError('accessToken not found')
@@ -39,13 +35,26 @@ export class EndpointPostExpress {
 			
 			this.endpointMongoDb.post(new SEDocument(collectionName, req.body)).then(
 				(docs: SEDocument[]) => {
-					this.resHandler.sendResponse(res, new BlapiResponse(docs));
+					this.handleResponse(res, docs, method.hook);
 				},
-				(error: BlError) => {
-					this.resHandler.sendErrorResponse(res, error.add(blError.msg('could not post document')
+				(postError: BlError) => {
+					this.resHandler.sendErrorResponse(res, new BlError('could not post document')
 						.store('url', url)
-						.store('body', req.body)));
+						.store('body', req.body)
+						.add(postError));
 				});
 		});
+	}
+	
+	private handleResponse(res: any, docs: SEDocument[], hook?: Hook) {
+		if (hook) {
+			hook.run(docs).then(() => {
+				this.resHandler.sendResponse(res, new BlapiResponse(docs));
+			}).catch((hookError: BlError) => {
+				this.resHandler.sendErrorResponse(res, new BlError('hook failed').add(hookError).code(800));
+			});
+		} else {
+			this.resHandler.sendResponse(res, new BlapiResponse(docs));
+		}
 	}
 }
