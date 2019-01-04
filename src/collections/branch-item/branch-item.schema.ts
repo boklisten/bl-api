e504c87ca677740ce3456488a9cfb8272f7f7796
holskil <aholskil@gmail.com>
2019-01-04 14:05:17 +0100

    fix: added new booleans to BranchItemSchema

--- src/collections/branch-item/branch-item.schema.ts
+++ src/collections/branch-item/branch-item.schema.ts
@@ -9,11 +9,16 @@ export const branchItemSchema = {
 	item: {
 		type: Schema.Types.ObjectId,
 		required: true
-	},
+  },
+
 	rent: {
 		type: Schema.Types.Boolean,
 		default: false
-	},
+  },
+  partlyPayment: {
+    type: Schema.Types.Boolean,
+    default: false
+  },
 	buy: {
 		type: Schema.Types.Boolean,
 		default: false
@@ -21,7 +26,33 @@ export const branchItemSchema = {
 	sell: {
 		type: Schema.Types.Boolean,
 		default: false
+  },
+  live: {
+    type: Schema.Types.Boolean,
+    default: false
+  },
+
+  rentAtBranch: {
+		type: Schema.Types.Boolean,
+		default: false
+  },
+  partlyPaymentAtBranch: {
+    type: Schema.Types.Boolean,
+    default: false
+  },
+	buyAtBranch: {
+		type: Schema.Types.Boolean,
+		default: false
 	},
+	sellAtBranch: {
+		type: Schema.Types.Boolean,
+		default: false
+  },
+  liveAtBranch: {
+    type: Schema.Types.Boolean,
+    default: false
+  },
+
 	sharedItems: {
 		type: [Schema.Types.ObjectId],
 		default: []
