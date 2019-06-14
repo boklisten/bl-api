0d62ef32d6a7a8332a923c79b364ecc294a5c66d
holskil <aholskil@gmail.com>
2019-06-14 16:43:15 +0200

    fix: added branch to getAll in user-detail collection

--- src/collections/user-detail/user-detail.collection.ts
+++ src/collections/user-detail/user-detail.collection.ts
@@ -1,71 +1,78 @@
-
-
-import {BlCollection, BlDocumentPermission, BlEndpoint, BlEndpointRestriction} from "../bl-collection";
-import {userDetailSchema} from "./user-detail.schema";
-import {Schema} from "mongoose";
-import {UserDetailValidOperation} from "./operations/user-detail-valid.operation";
+import {
+  BlCollection,
+  BlDocumentPermission,
+  BlEndpoint,
+  BlEndpointRestriction,
+} from '../bl-collection';
+import {userDetailSchema} from './user-detail.schema';
+import {Schema} from 'mongoose';
+import {UserDetailValidOperation} from './operations/user-detail-valid.operation';
 
 export class UserDetailCollection implements BlCollection {
-	collectionName = 'userdetails';
-	mongooseSchema = userDetailSchema;
-	documentPermission: BlDocumentPermission = {
-		viewableForPermission: "employee"
-	};
-	endpoints: BlEndpoint[] = [
-		{
-			method: 'getId',
-			restriction: {
-				permissions: ['customer', "employee", "manager", "admin", "super"],
-				restricted: true
-			},
-			operations: [
-				{
-					name: 'valid',
-					operation: new UserDetailValidOperation(),
-					restriction: {
-						permissions: ['customer', "employee", "manager", "admin", "super"],
-						restricted: true
-					}
-				}
-			]
-		},
-		{
-			method: 'patch',
-			restriction: {
-				permissions: ['customer', 'employee', "manager", "admin", "super"]
-			}
-		},
-		{
-			method: 'getAll',
-			validQueryParams: [
-				{
-					fieldName: 'email',
-					type: 'string'
-				},
-				{
-					fieldName: 'name',
-					type: "string"
-				},
-				{
-					fieldName: 'phone',
-					type: 'string'
-				},
-				{
-					fieldName: 'address',
-					type: 'string'
-				},
-				{
-					fieldName: 'postCity',
-					type: 'string'
-				},
-				{
-					fieldName: 'postCode',
-					type: 'string'
-				}
-			],
-			restriction: {
-				permissions: ['employee', "manager", 'admin', "super"]
-			}
-		}
-	]
+  collectionName = 'userdetails';
+  mongooseSchema = userDetailSchema;
+  documentPermission: BlDocumentPermission = {
+    viewableForPermission: 'employee',
+  };
+  endpoints: BlEndpoint[] = [
+    {
+      method: 'getId',
+      restriction: {
+        permissions: ['customer', 'employee', 'manager', 'admin', 'super'],
+        restricted: true,
+      },
+      operations: [
+        {
+          name: 'valid',
+          operation: new UserDetailValidOperation(),
+          restriction: {
+            permissions: ['customer', 'employee', 'manager', 'admin', 'super'],
+            restricted: true,
+          },
+        },
+      ],
+    },
+    {
+      method: 'patch',
+      restriction: {
+        permissions: ['customer', 'employee', 'manager', 'admin', 'super'],
+      },
+    },
+    {
+      method: 'getAll',
+      validQueryParams: [
+        {
+          fieldName: 'email',
+          type: 'string',
+        },
+        {
+          fieldName: 'branch',
+          type: 'object-id',
+        },
+        {
+          fieldName: 'name',
+          type: 'string',
+        },
+        {
+          fieldName: 'phone',
+          type: 'string',
+        },
+        {
+          fieldName: 'address',
+          type: 'string',
+        },
+        {
+          fieldName: 'postCity',
+          type: 'string',
+        },
+        {
+          fieldName: 'postCode',
+          type: 'string',
+        },
+      ],
+      restriction: {
+        permissions: ['employee', 'manager', 'admin', 'super'],
+      },
+    },
+  ];
 }
