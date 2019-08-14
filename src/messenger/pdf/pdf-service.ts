f8c2c86aeeac44ebb807bffcec3a0d2a3fbb8ee3
holskil <aholskil@gmail.com>
2019-08-14 13:12:06 +0100

    fix: moment utcOffset to be 120

--- src/messenger/pdf/pdf-service.ts
+++ src/messenger/pdf/pdf-service.ts
@@ -1,56 +1,81 @@
-import {EmailAttachment, EmailHandler, PdfHandler} from "@wizardcoder/bl-email";
-import {Order, UserDetail} from "@wizardcoder/bl-model";
-import {EmailSetting} from "@wizardcoder/bl-email/dist/ts/template/email-setting";
-import {EmailUser} from "@wizardcoder/bl-email/dist/ts/template/email-user";
-import {isNullOrUndefined} from "util";
-import moment = require("moment");
-import {EmailOrder} from "@wizardcoder/bl-email/dist/ts/template/email-order";
-import {OrderEmailHandler} from "../email/order-email/order-email-handler";
-
+import {EmailAttachment, EmailHandler, PdfHandler} from '@wizardcoder/bl-email';
+import {Order, UserDetail} from '@wizardcoder/bl-model';
+import {EmailSetting} from '@wizardcoder/bl-email/dist/ts/template/email-setting';
+import {EmailUser} from '@wizardcoder/bl-email/dist/ts/template/email-user';
+import {isNullOrUndefined} from 'util';
+import moment = require('moment');
+import {EmailOrder} from '@wizardcoder/bl-email/dist/ts/template/email-order';
+import {OrderEmailHandler} from '../email/order-email/order-email-handler';
 
 export class PdfService {
-	private _pdfHandler: PdfHandler;
-	private _standardDayFormat;
-	private _orderEmailHandler: OrderEmailHandler;
+  private _pdfHandler: PdfHandler;
+  private _standardDayFormat;
+  private _orderEmailHandler: OrderEmailHandler;
+  private _utcOffset: number;
 
-	constructor() {
-		const emailHandler = new EmailHandler({locale: 'nb'});
-		this._pdfHandler = new PdfHandler(emailHandler);
-		this._standardDayFormat = 'DD.MM.YYYY';
-		this._orderEmailHandler = new OrderEmailHandler(emailHandler);
-	}
+  constructor() {
+    const emailHandler = new EmailHandler({locale: 'nb'});
+    this._pdfHandler = new PdfHandler(emailHandler);
+    this._standardDayFormat = 'DD.MM.YYYY';
+    this._orderEmailHandler = new OrderEmailHandler(emailHandler);
+    this._utcOffset = 120;
+  }
 
-	async getOrderReceiptPdf(customerDetail: UserDetail, order: Order): Promise<EmailAttachment> {
-		let emailSetting = {} as EmailSetting;
+  async getOrderReceiptPdf(
+    customerDetail: UserDetail,
+    order: Order,
+  ): Promise<EmailAttachment> {
+    let emailSetting = {} as EmailSetting;
 
-		let emailUser: EmailUser = {
-			id: customerDetail.id,
-			dob: (!isNullOrUndefined(customerDetail.dob)) ? moment(customerDetail.dob).format(this._standardDayFormat) : '',
-			name: customerDetail.name,
-			email: customerDetail.email,
-			address: customerDetail.address
-		};
+    let emailUser: EmailUser = {
+      id: customerDetail.id,
+      dob: !isNullOrUndefined(customerDetail.dob)
+        ? moment(customerDetail.dob)
+            .utcOffset(this._utcOffset)
+            .format(this._standardDayFormat)
+        : '',
+      name: customerDetail.name,
+      email: customerDetail.email,
+      address: customerDetail.address,
+    };
 
-		let emailOrder: EmailOrder = await this._orderEmailHandler.orderToEmailOrder(order);
+    let emailOrder: EmailOrder = await this._orderEmailHandler.orderToEmailOrder(
+      order,
+    );
 
-		return await this._pdfHandler.getOrderReceipt(emailSetting, emailOrder, emailUser);
-	}
+    return await this._pdfHandler.getOrderReceipt(
+      emailSetting,
+      emailOrder,
+      emailUser,
+    );
+  }
 
-	async getOrderAgreementPdf(customerDetail: UserDetail, order: Order): Promise<EmailAttachment> {
-		let emailSetting = {} as EmailSetting;
+  async getOrderAgreementPdf(
+    customerDetail: UserDetail,
+    order: Order,
+  ): Promise<EmailAttachment> {
+    let emailSetting = {} as EmailSetting;
 
-		let emailUser: EmailUser = {
-			id: customerDetail.id,
-			dob: (!isNullOrUndefined(customerDetail.dob)) ? moment(customerDetail.dob).format(this._standardDayFormat) : '',
-			name: customerDetail.name,
-			email: customerDetail.email,
-			address: customerDetail.address
-		};
+    let emailUser: EmailUser = {
+      id: customerDetail.id,
+      dob: !isNullOrUndefined(customerDetail.dob)
+        ? moment(customerDetail.dob)
+            .utcOffset(this._utcOffset)
+            .format(this._standardDayFormat)
+        : '',
+      name: customerDetail.name,
+      email: customerDetail.email,
+      address: customerDetail.address,
+    };
 
-		let emailOrder: EmailOrder = await this._orderEmailHandler.orderToEmailOrder(order);
+    let emailOrder: EmailOrder = await this._orderEmailHandler.orderToEmailOrder(
+      order,
+    );
 
-		return await this._pdfHandler.getRentAgreement(emailSetting, emailOrder, emailUser);
-	}
-
-
+    return await this._pdfHandler.getRentAgreement(
+      emailSetting,
+      emailOrder,
+      emailUser,
+    );
+  }
 }
