3e8fd9393704dcb64890e723657ba04c7b3fba95
holskil <andreasholskil@protonmail.com>
2019-12-13 12:25:40 +0100

    fix: refresh token to expire in aprox 8 months

--- src/application-config.ts
+++ src/application-config.ts
@@ -1,109 +1,108 @@
-
 export const APP_CONFIG = {
-	path: {
-		client: {
-			checkout: 'cart/confirm',
-			agreement: {
-				rent: 'info/agreement/rent'
-			},
-			auth: {
-				failure: 'auth/authentication/failure',
-				socialLoginFailure: 'auth/social/failure'
-			}
-		},
-		dibs: {
-			payment: 'payments'
-		},
-		host: 'boklisten'
-	},
-	server: {
-		basePath: 'http://localhost:1337'
-	},
-	url: {
-		bring: {
-			shipmentInfo: 'https://api.bring.com/shippingguide/products/all.json'
-		},
-		blWeb: {
-			base: 'https://localhost:4200'
-		}
-	},
-	dev: {
-		server: {
-			host: 'https://localhost',
-			port: 1337,
-			path: 'api',
-			version: 'v1'
-		},
-		client: {
-			base: 'https://localhost:4200/'
-		},
-		mongoDb: {
-			basePath: 'mongodb://',
-			host: 'localhost',
-			port: 27017,
-			dbName: 'bl_dev_environment'
-		},
-		redis: {
-			basePath: '',
-			host: '',
-			port: 0,
-			dbName: ''
-		}
-	},
-	prod: {
-		server: {
-			host: '',
-			port: 0,
-			path: '',
-			version: ''
-		},
-		mongoDb: {
-			basePath: '',
-			host: '',
-			port: 0,
-			dbName: ''
-		},
-		redis: {
-			basePath: '',
-			host: '',
-			port: 0,
-			dbName: ''
-		}
-	},
-	test: true,
-	login: {
-		google: {
-			name: 'google'
-		},
-		facebook: {
-			name: 'facebook'
-		},
-		local: {
-			name: 'local'
-		}
-	},
-	token: {
-		refresh: {
-			iss: 'boklisten.co',
-			aud: 'boklisten.co',
-      expiresIn: "6 hours"
-		},
-		access: {
-			iss: 'boklisten.co',
-			aud: 'boklisten.co',
-      expiresIn: "10 minutes"
-		}
-	},
-	date: {
-		cancelDays: 14
-	},
-	payment: {
-		paymentServiceConfig: {
-			roundDown: true,
-			roundUp: false
-		}
-	},
-	delivery: {
-		deliveryDays: 7
-	}
+  path: {
+    client: {
+      checkout: 'cart/confirm',
+      agreement: {
+        rent: 'info/agreement/rent',
+      },
+      auth: {
+        failure: 'auth/authentication/failure',
+        socialLoginFailure: 'auth/social/failure',
+      },
+    },
+    dibs: {
+      payment: 'payments',
+    },
+    host: 'boklisten',
+  },
+  server: {
+    basePath: 'http://localhost:1337',
+  },
+  url: {
+    bring: {
+      shipmentInfo: 'https://api.bring.com/shippingguide/products/all.json',
+    },
+    blWeb: {
+      base: 'https://localhost:4200',
+    },
+  },
+  dev: {
+    server: {
+      host: 'https://localhost',
+      port: 1337,
+      path: 'api',
+      version: 'v1',
+    },
+    client: {
+      base: 'https://localhost:4200/',
+    },
+    mongoDb: {
+      basePath: 'mongodb://',
+      host: 'localhost',
+      port: 27017,
+      dbName: 'bl_dev_environment',
+    },
+    redis: {
+      basePath: '',
+      host: '',
+      port: 0,
+      dbName: '',
+    },
+  },
+  prod: {
+    server: {
+      host: '',
+      port: 0,
+      path: '',
+      version: '',
+    },
+    mongoDb: {
+      basePath: '',
+      host: '',
+      port: 0,
+      dbName: '',
+    },
+    redis: {
+      basePath: '',
+      host: '',
+      port: 0,
+      dbName: '',
+    },
+  },
+  test: true,
+  login: {
+    google: {
+      name: 'google',
+    },
+    facebook: {
+      name: 'facebook',
+    },
+    local: {
+      name: 'local',
+    },
+  },
+  token: {
+    refresh: {
+      iss: 'boklisten.co',
+      aud: 'boklisten.co',
+      expiresIn: '250d', // aprox 8 months
+    },
+    access: {
+      iss: 'boklisten.co',
+      aud: 'boklisten.co',
+      expiresIn: '10 minutes',
+    },
+  },
+  date: {
+    cancelDays: 14,
+  },
+  payment: {
+    paymentServiceConfig: {
+      roundDown: true,
+      roundUp: false,
+    },
+  },
+  delivery: {
+    deliveryDays: 7,
+  },
 };
