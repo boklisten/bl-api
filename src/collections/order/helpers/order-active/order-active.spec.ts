ea8c3923f4d49fe919a75df23e345341e3dc0933
holskil <andreasholskil@protonmail.com>
2020-08-06 15:40:14 +0200

    feat: orderActiveHelper for checking if a customer has active orders

---
+++ src/collections/order/helpers/order-active/order-active.spec.ts
@@ -0,0 +1,137 @@
+import "mocha";
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+import { expect } from "chai";
+import * as sinon from "sinon";
+import { BlError, Order } from "@wizardcoder/bl-model";
+import { BlDocumentStorage } from "../../../../storage/blDocumentStorage";
+import { OrderActive } from "./order-active";
+
+chai.use(chaiAsPromised);
+
+describe("OrderActive", () => {
+  const orderStorage = new BlDocumentStorage<Order>("orders");
+  const getOrderByQueryStub = sinon.stub(orderStorage, "getByQuery");
+  const orderActive = new OrderActive(orderStorage);
+  const testUserId = "5d765db5fc8c47001c408d8d";
+
+  beforeEach(() => {
+    getOrderByQueryStub.reset();
+  });
+
+  describe("haveActiveOrders()", () => {
+    it("should resolve with false if no orders was found", () => {
+      getOrderByQueryStub.rejects(new BlError("not found").code(702));
+
+      return expect(orderActive.haveActiveOrders(testUserId)).to.eventually.be
+        .false;
+    });
+
+    it("should resolve with false if orders was found but none was active", () => {
+      const nonActiveOrder: Order = {
+        id: "order1",
+        amount: 100,
+        orderItems: [],
+        branch: "branch1",
+        customer: testUserId,
+        byCustomer: true,
+        placed: false
+      };
+
+      getOrderByQueryStub.resolves([nonActiveOrder]);
+
+      return expect(orderActive.haveActiveOrders(testUserId)).to.eventually.be
+        .false;
+    });
+
+    it("should resolve with true if orders was found and at least one was active", () => {
+      const nonActiveOrder: Order = {
+        id: "order1",
+        amount: 100,
+        orderItems: [],
+        branch: "branch1",
+        customer: testUserId,
+        byCustomer: true,
+        placed: false
+      };
+
+      const activeOrder: Order = {
+        id: "order2",
+        amount: 200,
+        orderItems: [
+          {
+            type: "partly-payment",
+            item: "item1",
+            title: "title 1",
+            amount: 100,
+            unitPrice: 100,
+            taxRate: 0,
+            taxAmount: 0,
+            handout: false,
+            delivered: false
+          }
+        ],
+        branch: "branch1",
+        customer: testUserId,
+        byCustomer: true,
+        placed: true
+      };
+
+      getOrderByQueryStub.resolves([nonActiveOrder, activeOrder]);
+
+      return expect(orderActive.haveActiveOrders(testUserId)).to.eventually.be
+        .true;
+    });
+
+    it("should resolve with false if orders was found and all order-items was handed out", () => {
+      const nonActiveOrder: Order = {
+        id: "order1",
+        amount: 100,
+        orderItems: [
+          {
+            type: "partly-payment",
+            item: "item1",
+            title: "title 1",
+            amount: 100,
+            unitPrice: 100,
+            taxRate: 0,
+            taxAmount: 0,
+            handout: true,
+            delivered: false
+          }
+        ],
+        branch: "branch1",
+        customer: testUserId,
+        byCustomer: true,
+        placed: true
+      };
+
+      const nonActiveOrder2: Order = {
+        id: "order2",
+        amount: 200,
+        orderItems: [
+          {
+            type: "partly-payment",
+            item: "item1",
+            title: "title 1",
+            amount: 100,
+            unitPrice: 100,
+            taxRate: 0,
+            taxAmount: 0,
+            handout: true,
+            delivered: false
+          }
+        ],
+        branch: "branch1",
+        customer: testUserId,
+        byCustomer: true,
+        placed: true
+      };
+
+      getOrderByQueryStub.resolves([nonActiveOrder, nonActiveOrder2]);
+
+      return expect(orderActive.haveActiveOrders(testUserId)).to.eventually.be
+        .false;
+    });
+  });
+});
---
+++ src/collections/order/helpers/order-active/order-active.ts
@@ -0,0 +1,59 @@
+import { BlDocumentStorage } from "../../../../storage/blDocumentStorage";
+import { Order, OrderItem, BlError } from "@wizardcoder/bl-model";
+import { SEDbQueryBuilder } from "../../../../query/se.db-query-builder";
+
+export class OrderActive {
+  private _queryBuilder: SEDbQueryBuilder;
+
+  constructor(private _orderStorage: BlDocumentStorage<Order>) {
+    this._queryBuilder = new SEDbQueryBuilder();
+  }
+
+  public async haveActiveOrders(userId: string): Promise<boolean> {
+    const dbQuery = this._queryBuilder.getDbQuery({ customer: userId }, [
+      { fieldName: "customer", type: "object-id" }
+    ]);
+
+    let orders: Order[];
+
+    try {
+      orders = await this._orderStorage.getByQuery(dbQuery);
+    } catch (e) {
+      if (e instanceof BlError) {
+        if (e.getCode() === 702) {
+          return false;
+        }
+      }
+      throw e;
+    }
+
+    for (let order of orders) {
+      if (this.isOrderActive(order)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private isOrderActive(order: Order): boolean {
+    if (!order.placed) {
+      return false;
+    }
+
+    for (let orderItem of order.orderItems) {
+      if (this.isOrderItemActive(orderItem)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private isOrderItemActive(orderItem: OrderItem): boolean {
+    if (orderItem.handout || orderItem.delivered) {
+      return false;
+    }
+    return true;
+  }
+}
