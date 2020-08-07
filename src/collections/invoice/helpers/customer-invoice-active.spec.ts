499e3d0232e9d03645d8348d96eecc11131d9434
holskil <andreasholskil@protonmail.com>
2020-08-07 11:51:33 +0200

    feat: add CustomerInvoiceActive

---
+++ src/collections/invoice/helpers/customer-invoice-active.spec.ts
@@ -0,0 +1,83 @@
+import "mocha";
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+import { expect } from "chai";
+import * as sinon from "sinon";
+import { BlError, Invoice } from "@wizardcoder/bl-model";
+import { BlDocumentStorage } from "../../../storage/blDocumentStorage";
+import { CustomerInvoiceActive } from "./customer-invoice-active";
+chai.use(chaiAsPromised);
+
+describe("CustomerInvoiceActive", () => {
+  const invoiceStorage = new BlDocumentStorage<Invoice>("invoices");
+  const getInvoicesByQueryStub = sinon.stub(invoiceStorage, "getByQuery");
+  const customerInvoiceActive = new CustomerInvoiceActive(invoiceStorage);
+  const testUserId = "5f2aa6e8d39045001c444842";
+
+  describe("haveActiveInvoices()", () => {
+    it("should return false if no invoices was found for customer", () => {
+      getInvoicesByQueryStub.rejects(new BlError("not found").code(702));
+
+      return expect(customerInvoiceActive.haveActiveInvoices(testUserId)).to
+        .eventually.be.false;
+    });
+
+    it("should return false if invoices was found but none was active", () => {
+      const inactiveInvoice: Invoice = {
+        id: "invoice1",
+        duedate: new Date(),
+        customerHavePayed: true,
+        toDebtCollection: false,
+        toCreditNote: false,
+        customerItemPayments: [],
+        customerInfo: null,
+        payment: null
+      };
+
+      const inactiveInvoice2: Invoice = {
+        id: "invoice2",
+        duedate: new Date(),
+        customerHavePayed: false,
+        toDebtCollection: false,
+        toCreditNote: true,
+        customerItemPayments: [],
+        customerInfo: null,
+        payment: null
+      };
+
+      getInvoicesByQueryStub.resolves([inactiveInvoice, inactiveInvoice2]);
+
+      return expect(customerInvoiceActive.haveActiveInvoices(testUserId)).to
+        .eventually.be.false;
+    });
+
+    it("should return true if invoices was found and at least one was active", () => {
+      const inactiveInvoice: Invoice = {
+        id: "invoice1",
+        duedate: new Date(),
+        customerHavePayed: true,
+        toDebtCollection: false,
+        toCreditNote: false,
+        customerItemPayments: [],
+        customerInfo: null,
+        payment: null
+      };
+
+      const inactiveInvoice2: Invoice = {
+        id: "invoice2",
+        duedate: new Date(),
+        customerHavePayed: false,
+        toDebtCollection: false,
+        toCreditNote: false,
+        customerItemPayments: [],
+        customerInfo: null,
+        payment: null
+      };
+
+      getInvoicesByQueryStub.resolves([inactiveInvoice, inactiveInvoice2]);
+
+      return expect(customerInvoiceActive.haveActiveInvoices(testUserId)).to
+        .eventually.be.true;
+    });
+  });
+});
---
+++ src/collections/invoice/helpers/customer-invoice-active.ts
@@ -0,0 +1,43 @@
+import { BlDocumentStorage } from "../../../storage/blDocumentStorage";
+import { Invoice, BlError } from "@wizardcoder/bl-model";
+import { SEDbQueryBuilder } from "../../../query/se.db-query-builder";
+import { invoiceSchema } from "../invoice.schema";
+import { InvoiceActive } from "./invoice-active";
+
+export class CustomerInvoiceActive {
+  private queryBuilder: SEDbQueryBuilder;
+  private invoiceActive: InvoiceActive;
+
+  constructor(private invoiceStorage: BlDocumentStorage<Invoice>) {
+    this.invoiceStorage = this.invoiceStorage
+      ? this.invoiceStorage
+      : new BlDocumentStorage("invoices", invoiceSchema);
+    this.queryBuilder = new SEDbQueryBuilder();
+    this.invoiceActive = new InvoiceActive();
+  }
+
+  public async haveActiveInvoices(userId: string): Promise<boolean> {
+    const dbQuery = this.queryBuilder.getDbQuery(
+      { "customerInfo.userDetail": userId },
+      [{ fieldName: "customerInfo.userDetail", type: "object-id" }]
+    );
+    let invoices: Invoice[];
+    try {
+      invoices = await this.invoiceStorage.getByQuery(dbQuery);
+    } catch (e) {
+      if (e instanceof BlError) {
+        if (e.getCode() == 702) {
+          return false;
+        }
+      }
+    }
+
+    for (let invoice of invoices) {
+      if (this.invoiceActive.isActive(invoice)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
