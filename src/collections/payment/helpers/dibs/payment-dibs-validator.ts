fc3c0fa59280108dd3ea18759ff7be0f76dd90e3
holskil <aholskil@gmail.com>
2020-08-31 13:19:56 +0200

    fix: payment-dibs-validator to validate dibs payment

--- src/collections/payment/helpers/dibs/payment-dibs-validator.ts
+++ src/collections/payment/helpers/dibs/payment-dibs-validator.ts
@@ -1,85 +1,38 @@
 import {Order, Payment, AccessToken, BlError} from '@wizardcoder/bl-model';
 import {isNullOrUndefined} from 'util';
 import {DibsPaymentService} from '../../../../payment/dibs/dibs-payment.service';
-import {paymentSchema} from '../../payment.schema';
 import {BlDocumentStorage} from '../../../../storage/blDocumentStorage';
+import {DibsEasyPayment} from '../../../../payment/dibs/dibs-easy-payment/dibs-easy-payment';
 
 export class PaymentDibsValidator {
-  constructor(
-    private _dibsPaymentService?: DibsPaymentService,
-    private _paymentStorage?: BlDocumentStorage<Payment>,
-  ) {
+  constructor(private _dibsPaymentService?: DibsPaymentService) {
     this._dibsPaymentService = _dibsPaymentService
       ? _dibsPaymentService
       : new DibsPaymentService();
-
-    this._paymentStorage = _paymentStorage
-      ? _paymentStorage
-      : new BlDocumentStorage('payments', paymentSchema);
   }
 
   public async validate(
     order: Order,
     payment: Payment,
     accessToken: AccessToken,
-  ) {
-    if (isNullOrUndefined(payment.info)) {
-      throw new BlError(
-        'payment.method is "dibs" but payment.info is undefined',
-      );
-    }
+  ): Promise<boolean> {
+    this.validatePaymentInfo(payment);
 
-    if (isNullOrUndefined(payment.info['paymentId'])) {
-      throw new BlError(
-        'payment.method is "dibs" but payment.info.paymentId is undefined',
-      );
-    }
-
-    let dibsEasyPayment;
+    let dibsEasyPaymentDetails;
     try {
-      dibsEasyPayment = await this._dibsPaymentService.fetchDibsPaymentData(
+      dibsEasyPaymentDetails = await this._dibsPaymentService.fetchDibsPaymentData(
         payment.info['paymentId'],
       );
     } catch (getDibsPaymentError) {
-      throw new BlError('could not get dibs payment on dibs api').add(
+      throw new BlError('could not get dibs payment from dibs api').add(
         getDibsPaymentError,
       );
     }
 
-    if (isNullOrUndefined(dibsEasyPayment.orderDetails)) {
-      throw new BlError(
-        'dibsEasyPayment was found, but dibsEasyPayment.orderDetails was undefined',
-      )
-        .store('dibsEasyPayment', dibsEasyPayment)
-        .store('orderDetails', dibsEasyPayment['orderDetails']);
-    }
+    this.validateDibsEasyPayment(order, payment, dibsEasyPaymentDetails);
 
-    if (dibsEasyPayment.orderDetails.reference !== order.id) {
-      throw new BlError(
-        'dibsEasyPayment.orderDetails.reference is not equal to order.id',
-      ).store('dibsEasyPayment', dibsEasyPayment);
-    }
-
-    if (isNullOrUndefined(dibsEasyPayment.summary)) {
-      throw new BlError('dibsEasyPayment.summary is undefined').store(
-        'dibsEasyPayment',
-        dibsEasyPayment,
-      );
-    }
-
-    if (isNullOrUndefined(dibsEasyPayment.summary.reservedAmount)) {
-      throw new BlError(
-        'dibsEasyPayment.summary.reservedAmount is undefined',
-      ).store('dibsEasyPayment', dibsEasyPayment);
-    }
-
-    if (dibsEasyPayment.summary.reservedAmount !== payment.amount * 100) {
-      throw new BlError(
-        `dibsEasyPayment.summary.reservedAmount "${
-          dibsEasyPayment.summary.reservedAmount
-        }" is not equal to payment.amount "${payment.amount * 100}"`,
-      ).store('dibsEasyPayment', dibsEasyPayment);
-    }
+    return true;
+    /*
 
     let updatedPayment;
     try {
@@ -94,6 +47,7 @@ export class PaymentDibsValidator {
       );
     }
     return true;
+       */
     /*
     try {
       await this._userDetailHelper.updateUserDetailBasedOnDibsEasyPayment(
@@ -111,5 +65,47 @@ export class PaymentDibsValidator {
         .add(updateUserDetailError);
     }
        */
+  }
+
+  private validateDibsEasyPayment(
+    order: Order,
+    payment: Payment,
+    dibsEasyPaymentDetails: DibsEasyPayment,
+  ): boolean {
+    if (
+      isNullOrUndefined(dibsEasyPaymentDetails.orderDetails) ||
+      dibsEasyPaymentDetails.orderDetails.reference !== order.id
+    ) {
+      throw new BlError(
+        'dibsEasyPaymentDetails.orderDetails.reference is not equal to order.id',
+      );
+    }
+
+    if (
+      isNullOrUndefined(dibsEasyPaymentDetails.summary) ||
+      isNullOrUndefined(dibsEasyPaymentDetails.summary.reservedAmount) ||
+      parseInt('' + dibsEasyPaymentDetails.summary.reservedAmount, 10) !==
+        payment.amount * 100
+    ) {
+      throw new BlError(
+        `dibsEasyPaymentDetails.summary.reservedAmount "${
+          dibsEasyPaymentDetails.summary.reservedAmount
+        }" is not equal to payment.amount "${payment.amount * 100}"`,
+      );
+    }
+    return false;
+  }
+
+  private validatePaymentInfo(payment: Payment): boolean {
+    if (
+      isNullOrUndefined(payment.info) ||
+      isNullOrUndefined(payment.info['paymentId'])
+    ) {
+      throw new BlError(
+        'payment.method is "dibs" but payment.info.paymentId is undefined',
+      );
+    }
+
+    return true;
   }
 }
