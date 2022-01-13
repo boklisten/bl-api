e4f59fa800a5ef643d2e58e8b83a88d8925fc455
AdrianAndersen <adrianandersen@protonmail.com>
2022-01-13 21:25:24 +0100

    test: add fixtures for branches

--- src/messenger/pdf/pdf-service.ts
+++ src/messenger/pdf/pdf-service.ts
@@ -67,7 +67,8 @@ export class PdfService {
       address: customerDetail.address,
     };
 
-    const emailOrder: EmailOrder = await this._orderEmailHandler.orderToEmailOrder(order);
+    const emailOrder: EmailOrder =
+      await this._orderEmailHandler.orderToEmailOrder(order);
     return await this._pdfHandler.getRentAgreement(
       emailSetting,
       emailOrder,
