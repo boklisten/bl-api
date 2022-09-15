38af226348765eae465b0b24393fcd31f51c5cbc
Adrian Andersen <adrianandersen@protonmail.com>
2022-09-16 00:03:44 +0200

    feat(order-confirm): check that there are no duplicate orders

--- src/collections/order/operations/confirm/order-confirm.operation.ts
+++ src/collections/order/operations/confirm/order-confirm.operation.ts
@@ -6,13 +6,16 @@ import { orderSchema } from "../../order.schema";
 import { SEResponseHandler } from "../../../../response/se.response.handler";
 import {
   AccessToken,
-  BlError,
   BlapiResponse,
+  BlError,
   Order,
 } from "@boklisten/bl-model";
 import { OrderPlacedHandler } from "../../helpers/order-placed-handler/order-placed-handler";
+import { SEDbQueryBuilder } from "../../../../query/se.db-query-builder";
 
 export class OrderConfirmOperation implements Operation {
+  private _queryBuilder: SEDbQueryBuilder;
+
   constructor(
     private _resHandler?: SEResponseHandler,
     private _orderStorage?: BlDocumentStorage<Order>,
@@ -29,6 +32,40 @@ export class OrderConfirmOperation implements Operation {
     this._orderPlacedHandler = this._orderPlacedHandler
       ? this._orderPlacedHandler
       : new OrderPlacedHandler();
+
+    this._queryBuilder = new SEDbQueryBuilder();
+  }
+
+  private async hasDuplicateOrder(order: Order) {
+    const dbQuery = this._queryBuilder.getDbQuery(
+      { customer: order.customer, placed: "true" },
+      [
+        { fieldName: "customer", type: "object-id" },
+        { fieldName: "placed", type: "boolean" },
+      ]
+    );
+
+    try {
+      const existingOrders = await this._orderStorage.getByQuery(dbQuery);
+      return existingOrders
+        .filter(
+          (existingOrder) => existingOrder.orderItems.length === order.orderItems.length
+        )
+        .some((existingOrder) =>
+          order.orderItems.every((orderItem) =>
+            existingOrder.orderItems.some(
+              (existingOrderItem) =>
+                String(orderItem.item) === String(existingOrderItem.item) &&
+                orderItem.type === existingOrderItem.type &&
+                orderItem.info.to === existingOrderItem.info.to
+            )
+          )
+        );
+    } catch {
+      console.log("could not get user orders");
+    }
+
+    return false;
   }
 
   public async run(
@@ -49,6 +86,12 @@ export class OrderConfirmOperation implements Operation {
       order = await this._orderStorage.get(blApiRequest.documentId);
     } catch (e) {
       throw new BlError(`order "${blApiRequest.documentId}" not found`);
+    }
+
+    const isDuplicateOrder = await this.hasDuplicateOrder(order);
+
+    if (isDuplicateOrder) {
+      throw new BlError("There already exists an order with these orderitems");
     }
 
     let placedOrder;
