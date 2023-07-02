297fcdf1fd9008330cc9404783aaae2077473729
Adrian Andersen <adrianandersen@protonmail.com>
2023-07-02 15:46:25 +0200

    chore: bump all devDeps + TS to latest

--- src/collections/order/hooks/order.patch.hook.ts
+++ src/collections/order/hooks/order.patch.hook.ts
@@ -31,7 +31,7 @@ export class OrderPatchHook extends Hook {
     accessToken: AccessToken,
     id: string
   ): Promise<boolean> {
-    if (body === {} || typeof body == "undefined" || !body) {
+    if (typeof body == "undefined" || !body) {
       return Promise.reject(new BlError("body not defined"));
     }
 
