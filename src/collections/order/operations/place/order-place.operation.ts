af072aa944ecb999e44afb842222de3bbf33cbdd
Adrian Andersen <adrianandersen@protonmail.com>
2023-09-06 14:24:48 +0200

    fix(error): align error text

--- src/collections/order/operations/place/order-place.operation.ts
+++ src/collections/order/operations/place/order-place.operation.ts
@@ -354,7 +354,7 @@ export class OrderPlaceOperation implements Operation {
 
     if (someBlidAlreadyHandedOut) {
       throw new BlError(
-        "En eller flere av bøkene du prøver å dele ut er allerede aktiv på en annen kunde. Prøv å dele ut én og én bok for å finne synderen."
+        "En eller flere av bøkene du prøver å dele ut er allerede aktiv på en annen kunde. Prøv å dele ut én og én bok for å finne ut hvilke bøker dette gjelder."
       ).code(801);
     }
 
