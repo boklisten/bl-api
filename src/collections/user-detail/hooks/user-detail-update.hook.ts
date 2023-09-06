d7e544a8b22147541552653115f8817028b6815f
Lars Mitsem Selbekk <lars@selbekk.com>
2023-09-06 14:13:12 +0200

    fix(user-detail): validate input & capitalize hyphenated names
    Previously, the user-detail-update endpoint allowed replacing the
    entire UserDetail object as long as it's your own. Now, it only updates
    the fields you're allowed to.
    
    In addition, hyphenated names like "John Mayor-Taylor" are properly
    capitalized with an uppercase letter after the hyphen.

--- src/collections/user-detail/hooks/user-detail-update.hook.ts
+++ src/collections/user-detail/hooks/user-detail-update.hook.ts
@@ -1,31 +1,79 @@
 import { Hook } from "../../../hook/hook";
+import { AccessToken, BlError, UserDetail } from "@boklisten/bl-model";
 
 export class UserDetailUpdateHook extends Hook {
   private cleanUserInput = (dirtyText: string): string => {
-    return dirtyText
-      .replace(/\s+/gu, " ")
-      .trim()
-      .split(" ")
-      .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
-      .join(" ");
+    const withCoalescedSpaces = dirtyText.replace(/\s+/gu, " ").trim();
+    const separators = withCoalescedSpaces.match(/[ -]/g);
+    const caseCorrectedWordParts = withCoalescedSpaces
+      .split(/[ -]/g)
+      .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase());
+    return caseCorrectedWordParts
+      .map((part, index) => part + (separators?.[index] ?? ""))
+      .join("");
   };
 
   public override async before(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    body: any
-  ): Promise<boolean> {
-    if (body.name) {
-      body.name = this.cleanUserInput(body.name);
+    body: unknown,
+    accessToken: AccessToken
+  ): Promise<UserDetailPatch> {
+    if (!validateUserDetailUpdateType(body)) {
+      throw new BlError("Invalid UserDetailUpdateType request body").code(808);
+    }
+    const { name, address, postCity, dob, postCode, phone, emailConfirmed } =
+      body;
+    if (emailConfirmed !== undefined && accessToken.permission === "customer") {
+      throw new BlError(
+        "bruker kan ikke endre egen e-post-bekreftet-status"
+      ).code(910);
     }
 
-    if (body.address) {
-      body.address = this.cleanUserInput(body.address);
-    }
-
-    if (body.postCity) {
-      body.postCity = this.cleanUserInput(body.postCity);
-    }
-
-    return true;
+    // In an update call, a value of 'undefined' will remove a key, so the key
+    // needs to be completely missing if it shouldn't be updated.
+    return {
+      ...(name !== undefined && { name: this.cleanUserInput(name) }),
+      ...(address !== undefined && { address: this.cleanUserInput(address) }),
+      ...(postCity !== undefined && {
+        postCity: this.cleanUserInput(postCity),
+      }),
+      ...(dob !== undefined && { dob }),
+      ...(postCode !== undefined && { postCode }),
+      ...(phone !== undefined && { phone }),
+      ...(emailConfirmed !== undefined && { emailConfirmed }),
+    };
   }
 }
+
+export type UserDetailPatch = Partial<
+  Pick<
+    UserDetail,
+    "name" | "address" | "postCity" | "postCode" | "phone" | "emailConfirmed"
+  > & { dob: string } // DOB is otherwise Date
+>;
+
+const validateUserDetailUpdateType = (
+  candidate: unknown
+): candidate is UserDetailPatch => {
+  const _typeofTypeHelper = typeof ("" as unknown);
+  const isTypeOrUndefined = (
+    key: keyof UserDetailPatch,
+    typeName: typeof _typeofTypeHelper
+  ) => candidate[key] === undefined || typeof candidate[key] === typeName;
+
+  try {
+    const stringKeys = [
+      "name",
+      "address",
+      "postCity",
+      "dob",
+      "postCode",
+      "phone",
+    ] as const;
+    return (
+      stringKeys.every((key) => isTypeOrUndefined(key, "string")) &&
+      isTypeOrUndefined("emailConfirmed", "boolean")
+    );
+  } catch {
+    return false;
+  }
+};
