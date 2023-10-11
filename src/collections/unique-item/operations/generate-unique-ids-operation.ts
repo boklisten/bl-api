c8be6d7c580ce823b80dfdcccdd74f56d25ae123
Adrian Andersen <adrianandersen@protonmail.com>
2023-10-11 14:57:48 +0200

    feat(bl-id-generator): endpoint for id generation (#471)
    * feat(bl-id-generator): endpoint for id generation
    
    * fix(bl-id-generator): only one ID on each page
    
    * refactor(bl-id-generator): simplify generator funcs

---
+++ src/collections/unique-item/operations/generate-unique-ids-operation.ts
@@ -0,0 +1,24 @@
+import { Operation } from "../../../operation/operation";
+import { BlApiRequest } from "../../../request/bl-api-request";
+import { Request, Response } from "express";
+import { SEResponseHandler } from "../../../response/se.response.handler";
+import generateBlIdPDF from "../helpers/bl-id-generator";
+
+export class GenerateUniqueIdsOperation implements Operation {
+  constructor(private resHandler?: SEResponseHandler) {
+    this.resHandler ??= new SEResponseHandler();
+  }
+
+  async run(
+    blApiRequest: BlApiRequest,
+    req?: Request,
+    res?: Response
+  ): Promise<boolean> {
+    res.writeHead(200, {
+      "Content-Type": "application/pdf",
+    });
+
+    res.end(await generateBlIdPDF());
+    return true;
+  }
+}
