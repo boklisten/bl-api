fc9da69e0835c6d355aa5dfece73eb53501811d1
Adrian Andersen <adrianandersen@protonmail.com>
2024-06-14 11:13:44 +0200

    fix(sms-service): add twilio auto retries

--- src/messenger/sms/sms-service.ts
+++ src/messenger/sms/sms-service.ts
@@ -4,7 +4,10 @@ import { logger } from "../../logger/logger";
 
 const accountSid = process.env["TWILIO_SMS_SID"];
 const authToken = process.env["TWILIO_SMS_AUTH_TOKEN"];
-const client = twilio(accountSid ?? "ACSid", authToken ?? "authToken");
+const client = twilio(accountSid ?? "ACSid", authToken ?? "authToken", {
+  autoRetry: true,
+  maxRetries: 5,
+});
 
 /**
  * Send a single SMS to a single recipient
