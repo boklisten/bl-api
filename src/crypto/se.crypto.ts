730ff45a83d68e78dbc482ffe0026ac36fe6546f
Adrian Andersen <adrianandersen@protonmail.com>
2024-06-25 10:15:48 +0200

    deps(crypto): use esm imports

--- src/crypto/se.crypto.ts
+++ src/crypto/se.crypto.ts
@@ -1,7 +1,6 @@
-import { BlError } from "@boklisten/bl-model";
+import crypto from "crypto";
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const crypto = require("crypto");
+import { BlError } from "@boklisten/bl-model";
 
 export class SeCrypto {
   public cipher(msg: string): Promise<string> {
