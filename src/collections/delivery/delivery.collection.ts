67115a5253f3ff6e3268182c1382e8bc2eec6818
Adrian Andersen <adrianandersen@protonmail.com>
2024-07-05 12:47:19 +0200

    feat(postal-code): add postal code endpoint (#530)
    * feat(postal-code): add postal code endpoint
    
    * refactor(postal-code-lookup): use http handler and rename stuff
    
    * refactor(postal-code-lookup): adjust endpoint name

--- src/collections/delivery/delivery.collection.ts
+++ src/collections/delivery/delivery.collection.ts
@@ -1,6 +1,7 @@
 import { deliverySchema } from "./delivery.schema";
 import { DeliveryPatchHook } from "./hooks/delivery.patch.hook";
 import { DeliveryPostHook } from "./hooks/delivery.post.hook";
+import { PostalCodeLookupOperation } from "./operations/postal-code-lookup.operation";
 import { BlCollection, BlCollectionName, BlEndpoint } from "../bl-collection";
 
 export class DeliveryCollection implements BlCollection {
@@ -13,6 +14,12 @@ export class DeliveryCollection implements BlCollection {
       restriction: {
         permissions: ["customer", "employee", "manager", "admin", "super"],
       },
+      operations: [
+        {
+          name: "postal-code-lookup",
+          operation: new PostalCodeLookupOperation(),
+        },
+      ],
     },
     {
       method: "getAll",
